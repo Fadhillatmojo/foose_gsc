283a7d789dae8f2340a7634460e475ef28fd442a
User <fadhillatmojo@gmail.com>
2024-01-24 17:15:45 +0700

    register functional

--- lib/ui/pages/register_page.dart
+++ lib/ui/pages/register_page.dart
@@ -1,5 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:foose_gsc/models/models.dart';
 import 'package:foose_gsc/shared/colors.dart';
+import 'package:foose_gsc/ui/pages/article_page.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -11,6 +16,9 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   // form key
   final _formKey = GlobalKey<FormState>();
+
+  //auth firebase
+  final _auth = FirebaseAuth.instance;
 
   // editing controller
   final TextEditingController emailController = TextEditingController();
@@ -29,6 +37,19 @@ class _RegisterPageState extends State<RegisterPage> {
       keyboardType: TextInputType.emailAddress,
       onSaved: (value) {
         emailController.text = value!;
+      },
+      validator: (value) {
+        if (value!.isEmpty) {
+          return ("Please Enter Your Email");
+        }
+        // reg expression for email validation
+        // reg expression ini gunanya buat validasi saja
+        if (!RegExp("^[a-zA-Z0-9+_.-]+.@[a-zA-Z0-9.-]+.[a-z]")
+            .hasMatch(value)) {
+          return ("Please Enter A Valid Email");
+        }
+
+        return null;
       },
       style: const TextStyle(color: AppColors.primaryColor),
       cursorColor: AppColors.primaryColor,
@@ -50,6 +71,14 @@ class _RegisterPageState extends State<RegisterPage> {
           borderSide: const BorderSide(color: AppColors.primaryColor),
           borderRadius: BorderRadius.circular(10),
         ),
+        errorBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: Color.fromARGB(255, 179, 23, 12)),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: AppColors.primaryColor),
+          borderRadius: BorderRadius.circular(10),
+        ),
       ),
     );
 
@@ -60,6 +89,15 @@ class _RegisterPageState extends State<RegisterPage> {
       obscureText: true,
       onSaved: (value) {
         passwordController.text = value!;
+      },
+      validator: (value) {
+        RegExp regex = RegExp(r'^.{6,}$');
+        if (value!.isEmpty) {
+          return ("Password is Required for Login");
+        }
+        if (!regex.hasMatch(value)) {
+          return ("Please Enter Valid Password (Min. 6 Character)");
+        }
       },
       cursorColor: AppColors.primaryColor,
       textInputAction: TextInputAction.next,
@@ -81,6 +119,14 @@ class _RegisterPageState extends State<RegisterPage> {
           borderSide: const BorderSide(color: AppColors.primaryColor),
           borderRadius: BorderRadius.circular(10),
         ),
+        errorBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: Color.fromARGB(255, 179, 23, 12)),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: AppColors.primaryColor),
+          borderRadius: BorderRadius.circular(10),
+        ),
       ),
     );
 
@@ -91,6 +137,15 @@ class _RegisterPageState extends State<RegisterPage> {
       obscureText: true,
       onSaved: (value) {
         confirmPasswordController.text = value!;
+      },
+      validator: (value) {
+        if (value!.isEmpty) {
+          return ("Please Confirm Your Password");
+        }
+        if (confirmPasswordController.text != passwordController.text) {
+          return "Password don't match";
+        }
+        return null;
       },
       cursorColor: AppColors.primaryColor,
       textInputAction: TextInputAction.next,
@@ -112,6 +167,14 @@ class _RegisterPageState extends State<RegisterPage> {
           borderSide: const BorderSide(color: AppColors.primaryColor),
           borderRadius: BorderRadius.circular(10),
         ),
+        errorBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: Color.fromARGB(255, 179, 23, 12)),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: AppColors.primaryColor),
+          borderRadius: BorderRadius.circular(10),
+        ),
       ),
     );
 
@@ -122,10 +185,7 @@ class _RegisterPageState extends State<RegisterPage> {
       color: AppColors.accentColor,
       child: MaterialButton(
         onPressed: () {
-          // Navigator.push(
-          //   context,
-          //   MaterialPageRoute(builder: (context) => const HomeScreen()),
-          // );
+          signUp(emailController.text, confirmPasswordController.text);
         },
         padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
         minWidth: MediaQuery.of(context).size.width,
@@ -218,5 +278,46 @@ class _RegisterPageState extends State<RegisterPage> {
         ),
       ),
     );
+  }
+
+  // register function
+  void signUp(String email, String password) async {
+    if (_formKey.currentState!.validate()) {
+      try {
+        await _auth
+            .createUserWithEmailAndPassword(email: email, password: password)
+            .then((uid) => {postDetailsToFirestore()});
+      } catch (e) {
+        Fluttertoast.showToast(msg: e.toString());
+      }
+    }
+  }
+
+  void postDetailsToFirestore() async {
+    // calling firestore
+    //calling user model
+    // sending the values
+    FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+    User? user = _auth.currentUser;
+
+    UserModel userModel = UserModel();
+
+    // writing all the values
+    userModel.uid = user!.uid;
+    userModel.email = user.email;
+
+    await firebaseFirestore
+        .collection("users")
+        .doc(user.uid)
+        .set(userModel.toMap());
+
+    Fluttertoast.showToast(msg: "Account Created Successfully!");
+
+    if (!context.mounted) return;
+
+    Navigator.pushAndRemoveUntil(
+        (context),
+        MaterialPageRoute(builder: (context) => ArticlePage()),
+        (route) => false);
   }
 }
