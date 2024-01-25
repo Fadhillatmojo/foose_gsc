b20b5d0ebc4b063e4f22e4b0b9917950b8566e08
User <fadhillatmojo@gmail.com>
2024-01-25 19:10:42 +0700

    search bar

--- lib/ui/pages/article/article_page.dart
+++ lib/ui/pages/article/article_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:foose_gsc/shared/shared.dart';
 
 class ArticlePage extends StatefulWidget {
   const ArticlePage({super.key});
@@ -8,8 +9,50 @@ class ArticlePage extends StatefulWidget {
 }
 
 class _ArticlePageState extends State<ArticlePage> {
+  final TextEditingController searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('article'));
+    // email field untuk login
+    final searchField = TextFormField(
+      autofocus: false,
+      controller: searchController,
+      onSaved: (value) {
+        searchController.text = value!;
+      },
+      style: const TextStyle(color: AppColors.primaryColor),
+      cursorColor: AppColors.primaryColor,
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        prefixIcon: const Icon(Icons.search),
+        prefixIconColor: AppColors.primaryColor,
+        contentPadding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+        hintText: 'Search Article',
+        hintStyle: const TextStyle(
+          color: AppColors.primaryColor,
+          fontSize: 15,
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+          borderSide: const BorderSide(color: AppColors.primaryColor),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: AppColors.primaryColor),
+          borderRadius: BorderRadius.circular(30),
+        ),
+      ),
+    );
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              searchField,
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
