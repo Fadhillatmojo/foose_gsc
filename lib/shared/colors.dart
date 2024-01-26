bff6bb7c7aa66ab83fc5ac2730e0d2e9dfeea98c
User <fadhillatmojo@gmail.com>
2024-01-26 09:53:08 +0700

    update colors

--- lib/shared/colors.dart
+++ lib/shared/colors.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color primaryColor = Color(0xFF36914C);
   static const Color accentColor = Color(0xFF36914C);
+  static const Color darkColor = Color(0xFF38824A);
   static const Color greyColor = Color(0xFF92A0A9);
   static const Color backgroundColor = Color(0xFFE7E7E7);
 }
