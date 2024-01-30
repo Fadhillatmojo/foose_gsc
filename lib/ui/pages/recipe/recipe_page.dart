1df80e941826d38c685da69c2cade678fe975836
User <fadhillatmojo@gmail.com>
2024-01-30 19:44:59 +0700

    show search for recipe

--- lib/ui/pages/recipe/recipe_page.dart
+++ lib/ui/pages/recipe/recipe_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:foose_gsc/shared/shared.dart';
 
 class RecipePage extends StatefulWidget {
   const RecipePage({super.key});
@@ -8,8 +9,51 @@ class RecipePage extends StatefulWidget {
 }
 
 class _RecipePageState extends State<RecipePage> {
+  final TextEditingController searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('recipes'));
+    final searchField = TextFormField(
+      autofocus: false,
+      controller: searchController,
+      onSaved: (value) {
+        searchController.text = value!;
+      },
+      style: const TextStyle(color: AppColors.primaryColor),
+      cursorColor: AppColors.primaryColor,
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        prefixIcon: const Icon(Icons.search),
+        prefixIconColor: AppColors.primaryColor,
+        contentPadding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+        hintText: 'Search Recipe',
+        hintStyle: const TextStyle(
+          color: AppColors.primaryColor,
+          fontSize: 15,
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+          borderSide: const BorderSide(color: AppColors.primaryColor),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: AppColors.primaryColor),
+          borderRadius: BorderRadius.circular(30),
+        ),
+      ),
+    );
+
+    // page return build context
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              searchField,
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
